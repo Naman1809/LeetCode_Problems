e2b50984287e528aebda91f95ab91f0964ad36e8
Naman Kukreja <72972766+Naman1809@users.noreply.github.com>
2023-12-24 19:51:12 +0530

    Time: 5 ms (31.34%) | Memory: 8 MB (82.78%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
-void helper(map<int,int>&mp, vector<int>&nums,vector<vector<int>>&ans,vector<int>&res){
-    if(res.size()==nums.size()){
-        ans.push_back(res);
-        return;
-    }
-    for(int i=0;i<nums.size();i++){
-        if(!mp[nums[i]]){
-            mp[nums[i]]=1;
-            res.push_back(nums[i]);
-            helper(mp,nums,ans,res);
-            mp[nums[i]]=0;
-            res.pop_back();
-        }
-    }
+// void helper(map<int,int>&mp, vector<int>&nums,vector<vector<int>>&ans,vector<int>&res){
+//     if(res.size()==nums.size()){
+//         ans.push_back(res);
+//         return;
+//     }
+//     for(int i=0;i<nums.size();i++){
+//         if(!mp[nums[i]]){
+//             mp[nums[i]]=1;
+//             res.push_back(nums[i]);
+//             helper(mp,nums,ans,res);
+//             mp[nums[i]]=0;
+//             res.pop_back();
+//         }
+//     }
 
 
+// }
+void helper(int i, vector<int>&nums,vector<vector<int>>&ans,vector<int>&res){
+if(i==nums.size()){
+    ans.push_back(res);
+    return;
+}
+for(int j=i;j<nums.size();j++){
+    swap(nums[j],nums[i]);
+    res.push_back(nums[i]);
+    helper(i+1,nums,ans,res);
+    res.pop_back();
+    swap(nums[j],nums[i]);
+    
+}
 }
     vector<vector<int>> permute(vector<int>& nums) {
        vector<vector<int>>ans;
        vector<int>res;
        map<int,int>mp;
 
-       for(int i=0;i<nums.size();i++){
-       mp[nums[i]]=0;
-       } 
-           helper(mp,nums,ans,res);
+    //    for(int i=0;i<nums.size();i++){
+    //    mp[nums[i]]=0;
+    //    } 
+        //    helper(mp,nums,ans,res);
+        helper(0,nums,ans,res);
            return ans;
     }
 };
