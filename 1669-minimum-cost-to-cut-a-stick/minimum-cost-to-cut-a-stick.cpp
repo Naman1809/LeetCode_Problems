b84a04e05c02e32c57fdfd382403b830cae353e7
Naman Kukreja <72972766+Naman1809@users.noreply.github.com>
2024-08-08 22:51:52 +0530

    Time: 22 ms (82.99%) | Memory: 12.6 MB (26.32%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -18,7 +18,21 @@ public:
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>>dp(l+1,vector<int>(l+1,-1));
-        return helper(1,l,cuts,dp);
+        // vector<vector<int>>dp(l+1,vector<int>(l+1,-1));
+        vector<vector<int>>dp(l+2,vector<int>(l+2,0));
+
+        for(int i = l;i>=1;i--){
+            for(int j=1;j<=l;j++){
+                if(i>j)continue;
+                 int mini = INT_MAX;
+        for(int k=i;k<=j;k++){
+            int ans = cuts[j+1]-cuts[i-1] + dp[i][k-1]+ dp[k+1][j];
+            mini = min(ans, mini);
+        }
+        dp[i][j] = mini;
+            }
+        }
+        return dp[1][l];
+        // return helper(1,l,cuts,dp);
     }
 };
