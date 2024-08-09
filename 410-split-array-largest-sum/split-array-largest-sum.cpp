db278dbf1172864cd0237ec694cffa124a4048ec
Naman Kukreja <72972766+Naman1809@users.noreply.github.com>
2024-08-09 14:05:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (62.30%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int helper(vector<int>&nums,int mid){
+        int studentPage=0;
+        int student=0;
+        for(auto it :nums){
+            if(studentPage+it<=mid){
+                studentPage+=it;
+                
+            }else{
+                student++;
+                studentPage=it;
+            }
+        }
+        return student;
+
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        int maxi=0 ;
+        int sum =0;
+        for(auto it:nums){
+            maxi=max(it,maxi);
+            sum+=it;
+        }
+        int low = maxi,high =sum;
+        while(low<=high){
+            int mid  = low +(high-low)/2;
+            if(helper(nums,mid)>=k){
+                low=mid+1;
+            }else{
+                high = mid-1;
+            }
+        } 
+        return low;
+    }
+};
