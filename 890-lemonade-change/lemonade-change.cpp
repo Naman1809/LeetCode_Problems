b902d5066f46aa8a51ff73dfc9ea568e8781a01a
Naman Kukreja <72972766+Naman1809@users.noreply.github.com>
2024-08-15 09:27:27 +0530

    Time: 81 ms (19.33%) | Memory: 86 MB (32.71%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+         int a=0,b=0;
+    for(int i=0;i<bills.size();i++)
+    {
+        if(bills[i]==5) a++;
+         if(bills[i]==10)
+         {
+            if(a==0)
+              return false;
+            a--; b++;
+         }
+       else if(bills[i]==20)
+        {
+            if(a>0&&b>0){
+             a--; b--;}
+            else if(a>=3)
+               a-=3;
+            else
+            return false;
+        }
+    } 
+         return true;
+}
+    
+    
+};
